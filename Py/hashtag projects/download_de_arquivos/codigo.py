ce5905e95d5f4e47f88cb3827757870fad2a5d84
Vini <105316221+vini-muchulski@users.noreply.github.com>
2023-11-11 13:54:59 -0300

    Add files via upload

---
+++ Py/hashtag projects/download_de_arquivos/codigo.py
@@ -0,0 +1,10 @@
+import wget
+
+link = "https://stsci-opo.org/STScI-01HDHAWJAEK9N6EDK1YSPTSZE4.jpg"
+link_flicker = "https://www.flickr.com/photos/nasawebbtelescope/53321377829/in/dateposted/"
+wget.download(link, "foto.jpg")
+#wget.download(link_flicker, "foto_flicker.jpg")
+
+
+
+
