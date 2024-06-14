49f69306e29a1d7aeb01448eaaf7d3185c611be7
Vini <105316221+vini-muchulski@users.noreply.github.com>
2024-06-14 12:08:30 -0300

    Add files via upload

---
+++ mic_mic/dht_temp/esp_to_wifi/dht_temp_WIFI/estacao_meteorologica-main/estacao_meteorologica-main/main/main.ino
@@ -0,0 +1,76 @@
+#include "sensors/dht11.h"
+#include "sensors/bmp.h"
+#include "sensors/pluviometer.h"
+#include "sensors/anemometer.h"
+#include "sensors/windIndicator.h"
+
+// Pin definitions
+#define ANE_HALL_PIN 2  // ESP32 pin where the hall sensor is connected - Anemômetro SV10
+#define PLU_REED_PIN 13  // ESP32 pin where the magnetic sensor is connected - Pluviômetro
+#define DHT_PIN 15 // DHT11
+#define WIN_PIN 26 // Indicador de Vento DV10
+
+#define DELAY_TIME 1000  // Time between samples in milliseconds
+
+// Instantiate all sensor classes
+DhtSensor dht(DHT_PIN, DHT11);
+BmpSensor bmp;
+PluviometerSensor plu;
+AnemometerSensor ane;
+WindIndicatorSensor win(WIN_PIN);
+
+void setup() {
+  // Initialize the pins
+  pinMode(ANE_HALL_PIN, INPUT_PULLUP); // Set pin as input with internal pull-up
+  pinMode(PLU_REED_PIN, INPUT_PULLUP); // Set pin as input with internal pull-up
+
+  // Start serial communication
+  Serial.begin(9600);
+
+  // Initialize DHT11
+  dht.begin();
+  delay(DELAY_TIME);
+
+  // Initialize BMP280
+  bmp.begin(0x76);
+  delay(DELAY_TIME);
+  
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  
+  // Attach interrupt
+  attachInterrupt(digitalPinToInterrupt(ANE_HALL_PIN), countRevolution, RISING);
+  attachInterrupt(PLU_REED_PIN, isr_rain, FALLING);
+}
+
+void loop() {
+  String fullReport = "\n--------------\n"; 
+
+  fullReport += formatSensorOutput("DHT11", dht);
+  delay(DELAY_TIME);  
+
+  fullReport += formatSensorOutput("BMP280", bmp);
+  delay(DELAY_TIME);  
+
+  fullReport += formatSensorOutput("Pluviômetro", plu);
+  delay(DELAY_TIME);  
+
+  fullReport += formatSensorOutput("Anemômetro SV10", ane);
+  delay(DELAY_TIME);  
+
+  fullReport += formatSensorOutput("Indicador de Vento DV10", win);
+  delay(DELAY_TIME); 
+
+  fullReport += "--------------\n";
+
+  // Print full report on serial monitor
+  Serial.println(fullReport);
+}
+
+String formatSensorOutput(const String& name, Sensor& sensor) {
+  return "\n\n" + name + ":"+ "\n" + sensor.getSensorData();
+}
+
